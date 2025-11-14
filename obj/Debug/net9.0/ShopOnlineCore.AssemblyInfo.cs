73857a3187aa14733a48eb4ef0f351e91d520c13
Plakakice <loboplaka123@gmail.com>
2025-11-15 01:34:35 +0700

    Remove unused images and add new product images to the repository

--- obj/Debug/net9.0/ShopOnlineCore.AssemblyInfo.cs
+++ obj/Debug/net9.0/ShopOnlineCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopOnlineCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d9c92cf0edde0ceb887ce0e7d8dfeffd8da1ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+469c470e7232c36e024466d8203814f5da8de758")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopOnlineCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopOnlineCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
