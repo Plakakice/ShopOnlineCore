8126caa43840fcdf60f415a2158d27d2b1666e6a
Plakakice <loboplaka123@gmail.com>
2025-11-20 02:58:46 +0700

    Commit all changes before push

--- obj/Debug/net9.0/ShopOnlineCore.AssemblyInfo.cs
+++ obj/Debug/net9.0/ShopOnlineCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopOnlineCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96aeb1800839a61ebfe8f4ec1108f3be4a783572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0944f8d9b71743ebd6c2aafae688eb35795f0355")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopOnlineCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopOnlineCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
